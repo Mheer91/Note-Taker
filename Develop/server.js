04c6fdf246e6733036eb1de1a09dc30beeecc3f1
Michael Heer <mheer91@icloud.com>
2021-08-19 18:29:35 -0500

    All gets functuonal

---
+++ Develop/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const path = require('path');
+const notes = require('./db/db.json');
+// const db = require('../../index')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get('/api/notes', (req, res) => res.json(notes));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+
+
+
+app.listen(PORT, () =>
+  console.log(`Being my dad on port ${PORT}!`)
+);
+
+     
+// cd Documents/GitHub/Note-Taker/Develop/public
