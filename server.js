54b34f2d8c85858c4470ee45525d1d94a99ddcce
Michael Heer <mheer91@icloud.com>
2021-08-23 13:52:28 -0500

    Removed develop

--- server.js
+++ server.js
@@ -3,11 +3,22 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const path = require('path');
 const notes = require('./db/db.json');
+const { v4: uuidv4 } = require('uuid');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
 app.get('/api/notes', (req, res) => res.json(notes));
+
+app.post('/api/notes/', (req, res) => {
+
+const {title, text, noteID} = req.body;
+
+  if(req.params.noteID){
+    console.log(`${req.noteID}`)
+  }
+});
+
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
@@ -18,10 +29,9 @@ app.get('/*', (req, res) => {
   });
 
 
-
 app.listen(PORT, () =>
   console.log(`Being my dad on port ${PORT}!`)
 );
 
      
-// cd Documents/GitHub/Note-Taker/Develop/public
+// cd Documents/GitHub/Note-Taker/Develop
