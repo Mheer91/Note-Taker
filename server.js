6d4df81a0042d0f2f7b21a4eba25efd70207e4c5
Michael Heer <mheer91@icloud.com>
2021-08-23 17:17:33 -0500

    Mostly functional code

--- server.js
+++ server.js
@@ -4,34 +4,63 @@ const PORT = process.env.PORT || 3001;
 const path = require('path');
 const notes = require('./db/db.json');
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+
+
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.get('/api/notes', (req, res) => res.json(notes));
 
-app.post('/api/notes/', (req, res) => {
 
-const {title, text, noteID} = req.body;
+app.post('/api/notes', (req, res) => {
 
-  if(req.params.noteID){
-    console.log(`${req.noteID}`)
+  console.info(`${req.method} request received to add a review`);
+  const { title, text, noteId } = req.body;
+
+
+  const newNote = {
+    title,
+    text,
+    noteId: uuidv4(),
   }
-});
 
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
+  fs.readFile('./db/db.json', 'utf-8', (err, noteString) => {
+    if (err) {
+      return res.send("CRITICAL ERROR WITH FILE SYSTEM, SELF DISTRUCT IN 5 SECONDS")
+    }
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-  });
+    let readNotes = JSON.parse(noteString);
+    readNotes.push(newNote)
+    console.log(readNotes)
+
+    fs.writeFile(`./db/db.json`, JSON.stringify(readNotes), (err) => {
+      err
+        ? console.error(err)
+        : console.log(
+          `New note successfully added!` 
+        );
+    })
+    res.json(readNotes)
+  })
+})
+
+
+
 
 
 app.listen(PORT, () =>
-  console.log(`Being my dad on port ${PORT}!`)
+  console.log(`Express is litening to your fine, fine code at port ${PORT}!`)
 );
 
-     
-// cd Documents/GitHub/Note-Taker/Develop
+
